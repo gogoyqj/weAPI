114dc4b3339ce08519e9ff5e426a09d90e756f1d
gogoyqj <ivugogo@gmail.com>
2014-10-11 18:22:07 +0800

    Update weAPI.js

--- weAPI.js
+++ weAPI.js
@@ -1,83 +1,170 @@
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>wechat js api test - weAPI</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no,minimal-ui" name="viewport" />
-		<script src="weAPI.js"></script>
-		<style type="text/css">
-			body{
-				font-size: 20px;
-			}
-		</style>
-	</head>
-	<body>
-		<a href="#" onclick="weAPI.hideOptionMenu({},{done:function(){alert(3)}})">隐藏hideOptionMenu</a>
-		<a href="#" onclick="weAPI.hideToolbar({},{done:function(){alert(3)}})">隐藏toolbar</a>
-		<a href="#" onclick="weAPI.scanQRCode({},{done:function(){alert(3)}})">scan</a>
-		<a href="#" onclick="weAPI.closeWindow({},{done:function(){alert(3)}})">关闭</a>
-	</body>
-	<script>
-	 	function _log(msg) {
-	 		alert(msg)
-	 	}
-		weAPI.ready(function() {
-			weAPI.addPlugin({
-				key: "hideOptionMenu"
-			})
-			weAPI.addPlugin({
-				key: "hideToolbar"
-			})
-			// 这玩意也是有回调的
-			weAPI.addPlugin({
-				key: "closeWindow",
-				shareAction: true
-			})
-			weAPI.addPlugin({
-				key: "scanQRCode"
-			})
-			weAPI.addPlugin({
-				key: "shareWeibo",
-				shareAction: true,
-				dataList: {
-					content: "你好",
-					url: "http://qunar.com/"
-				}
-			})
-			weAPI.addPlugin({
-				key: "getNetworkType"
-			})
-			weAPI.addPlugin({
-				key: "shareTimeline",
-				shareAction: true,
-				dataList: {
-					"appid":"",
-		            "img_url":"",
-		            "link":"",
-		            "desc":"",
-		            "title":"",
-		            "img_width":"640",
-		            "img_height":"640"
-				}
+/**
+ * 	链式，weAPI是一个命名空间，weAPI.pluginName会创建一个plugin的实例，回调内的this都是这个plugin实例
+ * 	
+ */
+(function() {
+	"use strict"
 
+	function each(tar, func) {
+		for (var i in tar) {
+			func(i, tar[i])
+		}
+	}
+
+	function doNothing(d) {
+		return d
+	}
+
+	function isFunction(func) {
+		return typeof func === "function"
+	}
+
+	function excuteCBS(obj, arr, res) {
+		if(obj) {
+			each(arr, function(k, v) {
+				if(obj[k]) each(obj[k], function(i, func) {
+					func.call(obj, res)
+				})
 			})
-			weAPI.shareTimeline({
-				title:"yiwanpicaonima",
-				img_url: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg",
-				link: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg"
-			}).ready(function(){
-				_log("朋友圈ready")
-			}).done(function() {
-				_log("朋友圈done")
-			})
-			weAPI.shareWeibo({}).ready(function(){
-				_log("weibo ready")
-			}).done(function() {
-				_log("weibo done")
-			})
-			weAPI.getNetworkType({}).done(function(arg) {
-				alert(arg)
-			})
- 		})
-	</script>
-</html>
+		} 
+	}
+
+	var methods = ["ready", "cancel", "done", "success", "fail"]
+
+	
+	window.weAPI = {
+		/**
+		  *  @method addPlugin 用以添加新的插件
+		  *  @param {OBJECT} options 配置
+		  *  @p-options {string} key 插件名字
+		  *  @p-options {string} cmd 调用的微信接口名
+		  *  @p-options {function} resFormater 微信接口调用结束后，对响应数据的格式化函数，默认不做任何处理
+		  *  @p-options {function} dataFormater 传递给微信接口数据的格式化函数，默认不做任何处理
+		  *  @p-options {string} event 微信接口回调事件名，默认由cmd的驼峰形式转成:分隔
+		  *  @p-options {string} errMsg 微信err_msg的:前半部分，默认是cmd由驼峰模式转为下划线分隔
+		  *  @p-options {object} dataList 传递给接口的数据格式字段默认值
+		  */
+		addPlugin: function(options) {
+			var key = options.key,
+				resFormater = options.resFormater || doNothing,
+				dataFormater = options.dataFormater || doNothing,
+				dataList = options.dataList || {},
+				cmd = options.cmd || key,
+				notShareAction = !options.shareAction,
+				event = options.event || "menu:" + cmd.replace(/[A-Z]/g, function(mat) {
+					return ":" + mat.toLowerCase()
+				}),
+				errMsg = options.errMsg || cmd.replace(/[A-Z]/g, function(mat) {
+					return "_" + mat.toLowerCase()
+				})
+			if(!key) throw new Error("插件名字不能为空")
+			weAPI[key] = function(data, options) {
+				var me = new classObject(options, function() {
+					var me = this
+					me[key] = me.action = function(defineData) {
+						var newData = {}
+						defineData && each(dataList, function(key, value) {
+							newData[key] = defineData[key] === void 0 ? value : defineData[key]
+						})
+						// 格式化数据
+						newData = dataFormater(newData)
+						weAPI.exec(cmd, newData, function (resp) {
+							_log(cmd + " execute finished")
+							var callbackArr
+                			switch (resp.err_msg) {
+                				// 用户取消
+                				case errMsg + ':cancel':
+			                        callbackArr = "cancel"
+			                        break;
+			                    // 发送成功
+			                    case errMsg + ":confirm":
+			                    case errMsg + ":ok":
+			                        callbackArr = "success"
+			                        break;
+			                    // 发送失败
+			                    case errMsg + ":fail":
+			                    default:
+			                        callbackArr = "fail"
+			                        break;
+                			}
+                			// 格式化最终输出
+                			var res = resFormater(resp)
+                			excuteCBS(me, ["_" + callbackArr, "_done"], res)
+						})
+					}
+					// 关闭、隐藏之类的按钮
+					if(notShareAction) {
+						excuteCBS(me, ["_success", "_done"])
+					} else {
+						weAPI.on(event, function(argv) {
+			                // 就绪
+			                if(me._ready.length) each(me._ready, function(index, readyFunc) {
+			                	// 将data传递过去，方便在ready回调里面对data进行进一步处理
+			                	readyFunc.call(me, data, argv)
+			                })
+			                // 非异步情况
+			                // 异步数据通过回调内 this.action(newData)来调用微信接口
+			                // 异步数据不会进入到ready回调内
+			                if(!options.async) me[key](data);
+						})
+					}
+				}, notShareAction && key)
+				return me
+			}
+		},
+		on: function(event, callback) {
+			var api
+			if (!isFunction(callback)) callback = function() {}
+			return (api = window.WeixinJSBridge) != null ? api.on(event, callback) : void 0;
+		},
+		exec: function(cmd, data, callback) {
+			var api
+			if (data == null) data = {}
+			if (!isFunction(callback)) callback = function() {}
+			return (api = window.WeixinJSBridge) != null ? api.invoke(cmd, data, callback) : void 0;
+		}
+	}
+	/**
+	 *  @param {OBJECT} options 配置
+	 *  @param {FUNCTION} action 操作主函数
+	 */
+	function classObject(options, action, notShareAction) {
+		var me = this
+		each(methods, function(index, value) {
+			var cb = options && options[value]
+			me["_" + value] = cb && isFunction(cb) ? [cb] : []
+		})
+		if (isFunction(action)) action.call(me) 
+		if(notShareAction) me[notShareAction]()
+	}
+
+	each(methods, function(index, value) {
+		classObject.prototype[value] = function(callback) {
+			this["_" + value].push(callback)
+			return this
+		}
+	})
+
+	/**
+	 *  @method ready
+	 *  @param {FUNCTION} callback 微信js API ready后回调
+	 *  @p-options {OBJECT} weAPI 传递给回调函数的weAPI对象
+	 */
+	weAPI.ready = function(callback) {
+		if (isFunction(callback)) {
+			if (typeof window.WeixinJSBridge == "undefined" && /MicroMessenger/i.test(navigator.userAgent)) {
+				var newCallback = function() {
+					callback(weAPI)
+				}
+				if (document.addEventListener) {
+					document.addEventListener('WeixinJSBridgeReady', newCallback, false);
+				} else if (document.attachEvent) {
+					document.attachEvent('WeixinJSBridgeReady', newCallback);
+					document.attachEvent('onWeixinJSBridgeReady', newCallback);
+				}
+			} else {
+				callback(weAPI)
+			}
+		}
+	}
+})()

f275c1e54abb82273040dc62b360d3113538c99b
gogoyqj <ivugogo@gmail.com>
2014-10-21 19:20:51 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,2 @@
-{
-	"shareTimeline": {
-		"appid":"",
-    	"img_width":"640",
-    	"img_height":"640",
-		title:"有趣的图片",
-		desc:"这是一张搞笑的图片，呵呵呵呵",
-		img_url: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg",
-		link: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg"
-	},
-	"shareToFriend": {
-		title:"有趣的图片",
-		img_url: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg",
-		link: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg"
-	},
-	"shareWeibo": {
-		content: "有趣的图片",
-		url: "http://file5.u148.net/2014/10/images/1412934017UEH.jpg"
-	}
-}
+
+{"shareTimeline":{"appid":"","img_width":"640","img_height":"640",title:"有趣的图片",desc:"这是一张搞笑的图片，呵呵呵呵",img_url:"http://file5.u148.net/2014/10/images/1412934017UEH.jpg",link:"http://file5.u148.net/2014/10/images/1412934017UEH.jpg"},"shareToFriend":{title:"有趣的图片",img_url:"http://file5.u148.net/2014/10/images/1412934017UEH.jpg",link:"http://file5.u148.net/2014/10/images/1412934017UEH.jpg"},"shareWeibo":{content:"有趣的图片",url:"http://file5.u148.net/2014/10/images/1412934017UEH.jpg"}}
